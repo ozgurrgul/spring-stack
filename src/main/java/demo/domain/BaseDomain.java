2c490ab96885b8160ed6b4f9386eae94cc855748
Ozgur GUL <lugrugzo@gmail.com>
2017-08-12 13:25:20 +0300

    BaseDomain class added

---
+++ src/main/java/demo/domain/BaseDomain.java
@@ -0,0 +1,55 @@
+package demo.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Objects;
+
+@MappedSuperclass
+@Data
+@NoArgsConstructor
+public abstract class BaseDomain {
+
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid")
+    @Column(columnDefinition = "CHAR(32)")
+    @Id
+    protected String id;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", nullable = false)
+    private Date createdAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at", nullable = false)
+    private Date updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        updatedAt = createdAt = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = new Date();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        BaseDomain obj = (BaseDomain) o;
+
+        return Objects.equals(id, obj.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+}
